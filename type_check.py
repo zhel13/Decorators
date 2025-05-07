9a76014cd1e409c6808c621038de068f84cfc23d
Zhelyazko Dedov <109466030+zhel13@users.noreply.github.com>
2025-05-07 20:17:27 +0300

    Added solution from Softuni Exercise

---
+++ type_check.py
@@ -0,0 +1,15 @@
+def type_check(param_type):
+    def decorator(func):
+        def wrapper(param):
+            if not isinstance(param, param_type):
+                return 'Bad Type'
+            return func(param)
+        return wrapper
+    return decorator
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+print(times2(2))
+print(times2('Not A Number'))
